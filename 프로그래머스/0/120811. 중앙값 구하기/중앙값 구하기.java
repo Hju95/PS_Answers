3bc48f43ed0a43544695075f77c6e23f9dfae311
Jugwang Hong <59231743+Hju95@users.noreply.github.com>
2024-01-31 21:47:14 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.35 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        int n = array.length/2;
+        answer = array[n];
+        return answer;
+    }
+}
