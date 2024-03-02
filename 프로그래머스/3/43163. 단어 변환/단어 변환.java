35c7f3b21ecdc3951b9e6e76485ebb2bd871ce05
Jugwang Hong <59231743+Hju95@users.noreply.github.com>
2024-03-02 20:43:32 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    static int answer; 
+    static boolean[] visit;
+
+    public int solution(String begin, String target, String[] words) {
+        visit = new boolean[words.length];
+        dfs(begin, target, words, 0);
+        return answer;
+    }
+    
+    public void dfs(String begin, String target, String[] words, int count) {
+        if (begin.equals(target)) {
+            answer = count;
+            return;
+        }
+        
+        //단어를 하나하나 체크한다. 
+        //아직 visit처리가 안됐고, 알파벳 한개만 다를 때 (변경 가능) 변경해준다.
+        for (int i = 0; i< words.length; i++) {
+            if (visit[i] == true) 
+                continue;
+            
+            String word = words[i];
+            int notEqual = 0;
+            
+            for (int c = 0; c<begin.length(); c++) {
+                if (begin.charAt(c) != word.charAt(c)) {
+                    notEqual += 1;
+                }      
+            }
+            
+            //만약 1개만 다르면 변경 가능한 것
+            if (notEqual == 1) {
+                visit[i] = true;
+                dfs(word, target, words, count+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
