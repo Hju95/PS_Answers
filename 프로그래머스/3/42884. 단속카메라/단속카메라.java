d10f9ab04f6535dce1c5a95145ca312f4abee494
Jugwang Hong <remit_@naver.com>
2024-04-01 22:52:21 +0900

    [level 3] Title: 단속카메라, Time: 7.30 ms, Memory: 56.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+        
+        int count = 0;
+        int cameraPosition = Integer.MIN_VALUE;
+        
+        for (int[] route : routes) {
+            if (route[0] > cameraPosition) {
+                cameraPosition = route[1];
+                count++;
+            }
+        }
+        return count;
+    }
+}
